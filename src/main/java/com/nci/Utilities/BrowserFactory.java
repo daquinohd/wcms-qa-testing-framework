59918896711ae2dfa91c5d82d7e2272e54172c86
SupriyaSaxena0605 <SupriyaSaxena0605@users.noreply.github.com>
2017-11-05 21:04:45 -0500

    adding headless browser implementation

--- src/main/java/com/nci/Utilities/BrowserFactory.java
+++ src/main/java/com/nci/Utilities/BrowserFactory.java
@@ -2,7 +2,10 @@ package com.nci.Utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.apache.commons.lang3.SystemUtils;
 
 public class BrowserFactory {
@@ -48,7 +51,37 @@ public class BrowserFactory {
 			driver.manage().window().maximize();
 			driver.get(url);
 		}
-		
+		/*Headless Chrome */
+		if(browserName.equalsIgnoreCase("ChromeHeadless")){
+			System.out.println("chrome headless");
+			
+			String driverFullPath = getDriverPath(config, "ChromeDriver");
+
+			System.setProperty("webdriver.chrome.driver", driverFullPath);
+			System.out.println("Chrome Driver Path: " + driverFullPath);
+			ChromeOptions options=new ChromeOptions();
+			options.addArguments("headless");
+			driver=new ChromeDriver(options);
+			driver.manage().window().maximize();
+			driver.get(url);
+		}
+	
+		/*Headless Firefox*/
+		if(browserName.equalsIgnoreCase("FirefoxHeadless")){
+			System.out.println("Firefox headless");
+			 FirefoxBinary firefoxBinary = new FirefoxBinary(); 
+			 firefoxBinary.addCommandLineOptions("--headless"); 
+			 String driverFullPath = getDriverPath(config, "FirefoxDriver"); 
+			 System.setProperty("webdriver.gecko.driver", driverFullPath);
+			System.out.println("Firefox Driver Path: " + driverFullPath);
+			 FirefoxOptions firefoxOptions = new FirefoxOptions(); 
+			 firefoxOptions.setBinary(firefoxBinary); 
+			 driver = new FirefoxDriver(firefoxOptions);
+			 						
+			driver.manage().window().maximize();
+			driver.get(url);
+		}
+
 		return driver;
 	}
 }
