a8bd0486e070e32a3a9b92f0dca5dc45ea01c13e
Saxena <saxenas3@nih.gov>
2018-02-16 11:21:42 -0500

    Added more tests

---
+++ src/main/java/com/nci/clinicalTrial/pages/BasicSearchResults.java
@@ -0,0 +1,43 @@
+package com.nci.clinicalTrial.pages;
+
+import java.util.List;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasicSearchResults {
+	
+	WebDriver driver; 
+	public BasicSearchResults(WebDriver driver) 
+		{
+			this.driver = driver;
+			PageFactory.initElements(driver, this);
+			System.out.println("PageFactory initiated");
+		}
+	
+	//@FindBy(how=How.XPATH, using=".//input[@id='NCI-2014-01340' and @type='checkbox']") WebElement check_SearchTrials;
+	
+	
+	@FindBy(how=How.XPATH, using=".//div[@class='cts-checkbox checkbox']") List <WebElement> check_SearchTrials;
+	@FindBy(how=How.XPATH, using=".//input[@type='submit']") List <WebElement> btn_PrintSelected;
+	
+	public void selectCheckBox(){
+		
+		if (!((WebElement)check_SearchTrials.get(0)).isSelected()) 
+		{
+			((WebElement)check_SearchTrials.get(0)).click();
+		}
+		
+		System.out.println("Checkbox selected");
+	}
+
+	public void clickPrint(){
+		
+		((WebElement) btn_PrintSelected.get(0)).click();
+	}
+}
--- src/test/java/com/nci/testcases/AdvanceSearch_Test.java
+++ src/test/java/com/nci/testcases/AdvanceSearch_Test.java
@@ -55,14 +55,14 @@ public class AdvanceSearch_Test extends BaseClass {
 	public void verifyPageTitle()
 	{
 		Assert.assertEquals(advanceSearch.getAdvanceSearchPageTitle(), AdvanceSearch.ADVANCE_SEARCH_PAGE_TITLE);
-		//logger.log(LogStatus.PASS, "Verifying the Title of the page is *Find NCI-Supported Clinical Trials* | Actual Result: "+advanceSearch.getAdvanceSearchPageTitle());
+		logger.log(LogStatus.PASS, "Verifying the Title of the page is *Find NCI-Supported Clinical Trials* | Actual Result: "+advanceSearch.getAdvanceSearchPageTitle());
 	}
 	
 	@Test (groups = {"Smoke"}, priority = 1)  
 	public void verifyBanner()
 	{
 		banner.getBanner();
-		//logger.log(LogStatus.PASS, "Verifying the Banner of the page");
+		logger.log(LogStatus.PASS, "Verifying the Banner of the page");
 	}
 	
 	@Test (groups = {"Smoke"}, priority = 1)  
