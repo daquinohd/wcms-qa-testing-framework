ef6a8453d231c0378094fcee8922733309638305
daquinohd <daquinohd@mail.nih.gov>
2018-04-05 12:31:37 -0400

    Added proxy setup method & separated analytics test suite

--- src/test/java/com/nci/testcases/Analytics_Test.java
+++ src/test/java/com/nci/testcases/Analytics_Test.java
@@ -1,16 +1,22 @@
 package com.nci.testcases;
 
 import java.net.MalformedURLException;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import net.lightbody.bmp.BrowserMobProxy;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.client.ClientUtil;
+import net.lightbody.bmp.core.har.Har;
+import net.lightbody.bmp.proxy.CaptureType;
 
 import com.nci.Utilities.BrowserManager;
 import com.relevantcodes.extentreports.LogStatus;
@@ -21,7 +27,8 @@ public class Analytics_Test extends BaseClass {
 	// WebDriver driver;
 	AnalyticsLoad analytics;
 
-	// ConfigReader config = new ConfigReader();
+	// HAR file containing exported performance data
+	Har har;
 
 	@BeforeClass(groups = { "Smoke" })
 	@Parameters({ "browser" })
@@ -32,9 +39,43 @@ public class Analytics_Test extends BaseClass {
 		driver = BrowserManager.startBrowser(browser, pageURL);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		analytics = new AnalyticsLoad(driver);
+		//setupProxy();
 		System.out.println("Analytics setup done");
 	}
 
+	/**
+	 * Configure BrowserMob Proxy for Selenium
+	 * @throws RuntimeException
+	 */
+	public void setupProxy() throws RuntimeException {
+	    // start the proxy
+	    BrowserMobProxy proxy = new BrowserMobProxyServer();
+	    proxy.start(0);
+
+	    // get the Selenium proxy object
+	    Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
+
+	    // configure it as a desired capability
+	    DesiredCapabilities capabilities = new DesiredCapabilities();
+	    capabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
+
+	    // start the browser up
+	    WebDriver driver = new FirefoxDriver(capabilities);
+
+	    // enable more detailed HAR capture, if desired (see CaptureType for the complete list)
+	    proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
+
+	    // create a new HAR with the label "yahoo.com"
+	    proxy.newHar("cancer.gov");
+
+	    // open yahoo.com
+	    driver.get("https://www.cancer.gov");
+
+	    // get the HAR data
+	    har = proxy.getHar();
+		System.out.println("BMP proxy setup done");	    
+	}
+	
 	@Test(groups = { "Smoke" }, priority = 1)
 	public void veriFySAccount() {
 		String sAccountBlob = analytics.getSAccountText();
