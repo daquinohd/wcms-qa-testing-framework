6034e6cfb97c19726a85d280228365ee92e25a5e
Blair Learn <blairlearn@users.noreply.github.com>
2018-07-31 15:54:52 -0400

    Increase scroll delay and negative scroll
    Extra time for the megamenu to get out of the way.
    Ideally, the negative scroll amount would be a parameter....

--- src/main/java/gov/nci/Utilities/ScrollUtil.java
+++ src/main/java/gov/nci/Utilities/ScrollUtil.java
@@ -8,13 +8,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ScrollUtil {
 
-	static final int MAX_SCROLL_DELAY = 1;
+	static final int MAX_SCROLL_DELAY = 2;
 
 	public static void scrollIntoview(WebDriver driver, WebElement element) {
 
 		WebDriverWait wait = new WebDriverWait(driver, MAX_SCROLL_DELAY);
 
-		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);" + "window.scrollBy(0,-100);",
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);" + "window.scrollBy(0,-200);",
 				element);
 
 		wait.until(ExpectedConditions.visibilityOf(element));
