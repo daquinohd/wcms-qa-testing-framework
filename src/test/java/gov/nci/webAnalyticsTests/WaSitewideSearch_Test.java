c65c42abbc8f4761e44b89fbeb29d42642bc4a45
daquinohd <daquinohd@mail.nih.gov>
2018-08-15 16:37:14 -0400

    Added subsite URL and test; cleaned up data providers

--- src/test/java/gov/nci/webAnalyticsTests/WaSitewideSearch_Test.java
+++ src/test/java/gov/nci/webAnalyticsTests/WaSitewideSearch_Test.java
@@ -15,6 +15,7 @@ import gov.nci.Utilities.ExcelManager;
 public class WaSitewideSearch_Test extends AnalyticsTestBase {
 	
 	private final String TESTDATA_SHEET_NAME = "SitewideSearch";	
+	private final String TESTDATA_SHEET_NAME_ES = "SitewideSearchEs";
 	
 	private SitewideSearchForm swSearchForm;
 	String testDataFilePath;
@@ -27,6 +28,7 @@ public class WaSitewideSearch_Test extends AnalyticsTestBase {
 	// TODO: do we need to split this further
 	// TODO: separate page / component object creation
 	// TODO: Spanish sws
+	// TODO: Subsite sws
 	@BeforeMethod(groups = { "Analytics" }) 
 	public void beforeEachTest() {
 		driver.get(config.goHome());
@@ -37,10 +39,10 @@ public class WaSitewideSearch_Test extends AnalyticsTestBase {
 			ex.printStackTrace();
 		}
 	}
-	
-	// Test for search click events
-	@Test(dataProvider = "DefinitionTerms", groups = { "Analytics" }, priority = 1)
-	public void testSitewideSearchButtonClick(String searchTerm) {
+		
+	// Verify analytics click values for sitewide cancer term search
+	@Test(dataProvider = "CancerTerms", groups = { "Analytics" }, priority = 1)
+	public void testSitewideSearch(String searchTerm) {
 		try {
 		    swSearchForm.setSitewideSearchKeyword(searchTerm);
 		    swSearchForm.clickSearchButton();
@@ -58,8 +60,29 @@ public class WaSitewideSearch_Test extends AnalyticsTestBase {
 		}
 	}
 	
-	// Test for unmatched search click
-	@Test(dataProvider = "NoMatchTerms", groups = { "Analytics" }, priority = 2)
+	// Verify analytics click values for sitewide Spanish term search
+	@Test(dataProvider = "CancerTermsEs", groups = { "Analytics" }, priority = 2)
+	public void testSitewideSearchEspanol(String searchTerm) {
+		try {
+			driver.get(config.getPageURL("SpanishPage"));
+		    swSearchForm.setSitewideSearchKeyword(searchTerm);
+		    swSearchForm.clickSearchButton();
+		    setClickBeacon(); 
+			Assert.assertTrue(hasEvent(2), "Event value incorrect.");
+			Assert.assertTrue(hasProp(4, "d=pev1"), "Value of prop4 incorrect.");
+			Assert.assertTrue(hasProp(11, "sitewide"), "Search type value incorrect.");
+			Assert.assertTrue(hasProp(14, searchTerm), "Search term value does not match.");
+			Assert.assertTrue(hasProp(67, "D=pageName"), "Dynamic pageName value incorrect.");
+			Assert.assertTrue(haseVar(13), "eVar13 value incorrect.");
+			Assert.assertTrue(haseVar(14, searchTerm), "Search type value incorrect.");
+		} catch (Exception e) {
+			Assert.fail("Error submitting sitewide search.");
+			e.printStackTrace();
+		}
+	}
+
+	// Verify analytics click values for sitewide search w/o results
+	@Test(dataProvider = "NoMatchTerms", groups = { "Analytics" }, priority = 3)
 	public void testSitewideSearchNoMatch(String searchTerm) {
 		try {
 		    swSearchForm.setSitewideSearchKeyword(searchTerm);
@@ -76,71 +99,13 @@ public class WaSitewideSearch_Test extends AnalyticsTestBase {
 			Assert.fail("Error submitting sitewide search.");
 			e.printStackTrace();
 		}
-	}	
-	
-	// Test for searchresults load events
-	@Test(dataProvider = "OtherTerms", groups = { "Analytics" }, priority = 3)
-	public void testSitewideSearchResultsLoad(String searchTerm) {
-		try {
-		    swSearchForm.setSitewideSearchKeyword(searchTerm);
-		    swSearchForm.clickSearchButton();
-		    setLoadBeacon();
-			Assert.assertTrue(hasEvent(1), "Event value incorrect.");
-			Assert.assertTrue(hasEvent(47), "Event value incorrect.");
-			Assert.assertFalse(hasEvent(2), "Unexpected event value.");
-			Assert.assertTrue(hasProp(44, "Global search"), "Search type value incorrect");
-			// Assert.assertTrue(hasHier()); // TODO: fix hier check
-		} catch (Exception e) {
-			Assert.fail("Error loading sitewide search results page.");
-			e.printStackTrace();
-		}
 	}
 	
-	// Test for searchresults load events
-	@Test(groups = { "Analytics" }, priority = 4)
-	public void testNoResultsOnce() {
+	// Verify analytics click values for microsite-wide search
+	@Test(dataProvider = "CancerTerms", groups = { "Analytics" }, priority = 5)
+	public void testSitewideSearchButtonClick(String searchTerm) {
 		try {
-		    swSearchForm.setSitewideSearchKeyword("wingardium leviosa");
-		    swSearchForm.clickSearchButton();
-		    setLoadBeacon();
-			Assert.assertTrue(hasEvent(1), "Event value incorrect.");
-			Assert.assertTrue(hasEvent(47), "Event value incorrect.");
-			Assert.assertFalse(hasEvent(2), "Unexpected event value.");
-			Assert.assertTrue(hasProp(44, "Global search"), "Search type value incorrect");
-			// Assert.assertTrue(hasHier()); // TODO: fix hier check
-		} catch (Exception e) {
-			Assert.fail("Error loading sitewide search results page.");
-			e.printStackTrace();
-		}
-	}	
-	
-	// Test for searchresults load events
-	@Test(groups = { "Analytics" }, priority = 5)
-	public void testAllSitewideSearchResultsLoad() {
-		try {
-		    swSearchForm.setSitewideSearchKeyword("");
-		    swSearchForm.clickSearchButton();
-		    setLoadBeacon();
-			Assert.assertTrue(hasEvent(1), "Event value incorrect.");
-			Assert.assertTrue(hasEvent(47), "Event value incorrect.");
-			Assert.assertFalse(hasEvent(2), "Unexpected event value.");
-			Assert.assertTrue(hasProp(44, "Global search"), "Search type value incorrect");
-		} catch (Exception e) {
-			Assert.fail("Error loading sitewide search results page.");
-			e.printStackTrace();
-		}
-	}	
-	
-	// Test for search click events
-	/**
-	* TODO: fix NoSuchSessionException()
-	* This only seems to break when the data provider is used AND
-	* when it's the final test on the list
-	* The driver may be quitting before expected
-	*/	
-	@Test(dataProvider = "DefinitionTerms", groups = { "Analytics" }, priority = 6)
-	public void itBreaksHere(String searchTerm) {
-		try {
+			driver.get(config.getPageURL("MicroSite"));			
 		    swSearchForm.setSitewideSearchKeyword(searchTerm);
 		    swSearchForm.clickSearchButton();
 		    setClickBeacon();
@@ -156,41 +121,37 @@ public class WaSitewideSearch_Test extends AnalyticsTestBase {
 			e.printStackTrace();
 		}
 	}
+		
+	/**** 
+	 * TODO tests 
+	 * Results page (separate test?)
+	 * Form on results page
+	 * 
+	 ****/	
 	
 	/******************** Data Providers ****************/		
 
 	@DataProvider(name = "CancerTerms")
 	public Iterator<Object[]> readCancerTerm_Data() {
-		return getDataIteratorObject("CancerTerm");
+		return getDataIteratorObject("CancerTerm", TESTDATA_SHEET_NAME);
 	}
 
-	@DataProvider(name = "BestBetTerms")
-	public Iterator<Object[]> readBestBetTerm_Data() {
-		return getDataIteratorObject("BestBet");
+	@DataProvider(name = "CancerTermsEs")
+	public Iterator<Object[]> readCancerTermEs_Data() {
+		return getDataIteratorObject("CancerTerm", TESTDATA_SHEET_NAME_ES);
 	}
-	
-	@DataProvider(name = "DefinitionTerms")
-	public Iterator<Object[]> readDefinitionTerm_Data() {
-		return getDataIteratorObject("Definition");
-	}
-	
-	@DataProvider(name = "OtherTerms")
-	public Iterator<Object[]> readOtherTerm_Data() {
-		return getDataIteratorObject("Other");
-	}
-	
+		
 	@DataProvider(name = "NoMatchTerms")
 	public Iterator<Object[]> readNoMatchTerm_Data() {
-		return getDataIteratorObject("NoMatch");
+		return getDataIteratorObject("NoMatch", TESTDATA_SHEET_NAME);
 	}
 	
-	private Iterator<Object[]> getDataIteratorObject(String columnName) {
+	private Iterator<Object[]> getDataIteratorObject(String columnName, String sheetName) {
 		ExcelManager excelReader = new ExcelManager(testDataFilePath);
 		ArrayList<Object[]> myObjects = new ArrayList<Object[]>();
-		for (int rowNum = 2; rowNum <= excelReader.getRowCount(TESTDATA_SHEET_NAME); rowNum++) {
-			String searchTerm = excelReader.getCellData(TESTDATA_SHEET_NAME, columnName, rowNum);
+		for (int rowNum = 2; rowNum <= excelReader.getRowCount(sheetName); rowNum++) {
+			String searchTerm = excelReader.getCellData(sheetName, columnName, rowNum);
 			if(!searchTerm.isEmpty()) {
-				System.out.println("Sitewide search term ======= " + searchTerm);
 				Object ob[] = { searchTerm };
 				myObjects.add(ob);
 			}
