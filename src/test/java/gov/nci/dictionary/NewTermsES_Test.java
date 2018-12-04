e1fa60d7ebc7c8c4c98b16e594861ba8f7158d87
Volker Englisch <venglisch@users.noreply.github.com>
2018-12-04 13:13:55 -0500

    GlossaryES - Test if search button is visible. OCEPROJECT-4800 (#165)

--- src/test/java/gov/nci/dictionary/NewTermsES_Test.java
+++ src/test/java/gov/nci/dictionary/NewTermsES_Test.java
@@ -154,6 +154,28 @@ public class NewTermsES_Test extends NewDictionaryCommon {
         }
     }
 
+
+    // Confirming the Search button is displayed
+    // -------------------------------------------------------------------------
+    @Test(dataProvider = "GlossaryES", groups = { "dictionary" })
+    public void SearchBtnVisible(String url) {
+        DictionarySearch dict;
+        String curMethod = new Object(){}.getClass().getEnclosingMethod().getName();
+
+        logger.log(LogStatus.INFO, "Testing if Search button is visible");
+        driver.get(url);
+
+        try {
+            dict = new DictionarySearch(driver);
+            boolean searchBtnVisible = dict.isSearchBtnVisible();
+            String searchBtnNotVisibleTxt = "*** Error: Glossary (ES) Dictionary "
+                                          + "search button Not Displayed ***";
+            Assert.assertTrue(searchBtnVisible, searchBtnNotVisibleTxt);
+        } catch (MalformedURLException | UnsupportedEncodingException e) {
+            Assert.fail("*** Error loading page in " + curMethod + " ***");
+        }
+    }
+
 /*  ***************************** Data Provider ***************************************** */
 
     // DataProvider to read the Excel spreadsheet with data containing URLs to be checked
