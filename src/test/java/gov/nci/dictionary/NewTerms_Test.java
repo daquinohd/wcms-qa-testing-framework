d4290b04cdfa2078b2c3b7c0635a0032a11b1ec7
Volker Englisch <venglisch@users.noreply.github.com>
2018-12-04 12:10:33 -0500

    Glossary - Test search input field is visible. OCEPROJECT-4786 (#159)

--- src/test/java/gov/nci/dictionary/NewTerms_Test.java
+++ src/test/java/gov/nci/dictionary/NewTerms_Test.java
@@ -151,6 +151,27 @@ public class NewTerms_Test extends NewDictionaryCommon {
             Assert.fail("*** Error loading page in " + curMethod + " ***");
         }
     }
+    // Confirming the search input field is displayed
+    // -------------------------------------------------------------------------
+    @Test(dataProvider = "Glossary", groups = { "dictionary" })
+    public void SearchInputVisible(String url) {
+        DictionarySearch dict;
+        String curMethod = new Object(){}.getClass().getEnclosingMethod().getName();
+
+        logger.log(LogStatus.INFO, "Testing if search entry field is visible");
+        driver.get(url);
+
+        try {
+            dict = new DictionarySearch(driver);
+            boolean searchVisible = dict.isSearchInputVisible();
+            String searchErrorTxt = "*** Error: Glossary Search Input "
+                                  + "Field Not Displayed ***";
+            Assert.assertTrue(searchVisible, searchErrorTxt);
+        } catch (MalformedURLException | UnsupportedEncodingException e) {
+            Assert.fail("*** Error loading page in " + curMethod + " ***");
+        }
+    }
+
 /*  ***************************** Data Provider ***************************************** */
 
     // DataProvider to read the Excel spreadsheet with data containing URLs to be checked
