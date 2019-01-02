4fa2d617a9833bb23da3c86a03725712b65d673b
Nadia <alinai@users.noreply.github.com>
2019-01-02 09:26:19 -0500

    Added test to verify the url of the fact sheet

--- src/main/java/gov/nci/factsheets/FactSheetsListPage.java
+++ src/main/java/gov/nci/factsheets/FactSheetsListPage.java
@@ -1,11 +1,14 @@
 package gov.nci.factsheets;
 
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+
 import com.relevantcodes.extentreports.ExtentTest;
 
 
@@ -16,15 +19,20 @@ public class FactSheetsListPage {
 	ExtentTest logger;
 
 
-	
+
 /**************** FactSheets Page WebElements *****************************/
-	@FindBy(how = How.CSS, using = "div.resize-content>h1>span")   
+	@FindBy(how = How.CSS, using = "div.resize-content>h1>span")
 	   WebElement factsheetslist_PageTitle;
 	@FindBy(how = How.CSS, using = "#cgvBody>div.slot-item.first-SI>p")
 	    WebElement text_FSListIntroText;
 	@FindBy(how = How.CSS, using = "head#header>title")
 		WebElement factsheetslist_BrowserTitle;
-	
+	@FindBy(how = How.CSS, using = "ul.generic-bullet>li>a")
+	List<WebElement> subjectListItems ;
+
+
+
+
 // Initializing the Page Objects
 	    public FactSheetsListPage(WebDriver driver, ExtentTest logger) {
 			this.driver = driver;
@@ -34,15 +42,16 @@ public class FactSheetsListPage {
 
 // Testing if the H1 header element exists on the page
 // ---------------------------------------------------
-	    
+
 	    public WebElement getPageH1Title() {
 			return factsheetslist_PageTitle;
 		}
 }
 
-	    
-		
-			
-			
-		
-		
+
+
+
+
+
+
+
--- src/test/java/gov/nci/factsheets/Tests/factsheetslistpage_Test.java
+++ src/test/java/gov/nci/factsheets/Tests/factsheetslistpage_Test.java
@@ -1,17 +1,21 @@
 package gov.nci.factsheets.Tests;
 
-import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.google.common.collect.Lists;
 import com.relevantcodes.extentreports.LogStatus;
+
 import gov.nci.Utilities.BrowserManager;
 import gov.nci.clinicaltrials.BaseClass;
+import gov.nci.commonobjects.Banner;
+import gov.nci.commonobjects.BreadCrumb;
 import gov.nci.factsheets.FactSheetsListPage;
-import com.google.common.collect.Lists;
 
 
 
@@ -21,12 +25,15 @@ public class factsheetslistpage_Test extends BaseClass {
     public static final String FACTSHEETLISTPAGE_PAGE_TITLE = "NCI Fact Sheets";
 	public static final String FACTSHEETSLISTPAGE_INTRO_TEXT = "The NCI fact sheet collection addresses a variety of cancer topics. Fact sheets are updated and revised based on the latest cancer research.";
     public final String BREAD_CRUMB = "Home\nPublications";
-    public static final List<String> FACTSHEETLISTS_ON_FACTSHEETSLISTPAGE= Lists.newArrayList("Cancer Therapy","Cancer Types",
+    public static final String FACTSHEETLISTPAGE_URL = "https://www-qa.cancer.gov/publications/fact-sheets";
+    public static final List<String> EXP_FACTSHEETLISTS_ON_FACTSHEETSLISTPAGE= Lists.newArrayList("Cancer Therapy","Cancer Types",
 			"Detection and Diagnosis", "Diet and Nutrition", "Prevention", "Risk Factors and Possible Causes", "Support, Coping, and Resources",
 			"Tobacco and Smoking Cessation", "En Español");
-	
-	FactSheetsListPage fslp;
-	
+
+    FactSheetsListPage fslp;
+	BreadCrumb crumb;
+	Banner banner;
+
 
 	@BeforeClass
 	@Parameters({ "browser" })
@@ -36,9 +43,7 @@ public class factsheetslistpage_Test extends BaseClass {
 		driver = BrowserManager.startBrowser(browser, config, pageURL);
 		System.out.println("FactSheetsList Page setup done");
 	}
-	
-	
-	
+
 	@Test
 	public void verifyFSlistPageBrowserTitle() {
 		driver.get(pageURL);
@@ -46,17 +51,23 @@ public class factsheetslistpage_Test extends BaseClass {
 		Assert.assertEquals(driver.getTitle(), FACTSHEETLISTPAGE_Browser_TITLE);
 	logger.log(LogStatus.PASS, "Verify that Browser Title of the page is *Fact Sheets - National Cancer Institute* | Actual Result: "
 				+ driver.getTitle());
-	
 	}
-	
-	
-		
-	@Test
+
+    @Test
 	public void verifyFSlistPageH1Title() {
 		driver.get(pageURL);
 		fslp = new FactSheetsListPage(driver, logger);
 		Assert.assertEquals(fslp.getPageH1Title().getText(),FACTSHEETLISTPAGE_PAGE_TITLE);
 		logger.log(LogStatus.PASS, "Verify that H1 Title of the page is *NCI Fact Sheets* | Actual Result: "
 				+ fslp.getPageH1Title());
-	}
+		}
+
+    @Test
+    public void verifyFSlistPageURL() {
+    	driver.get(pageURL);
+	    fslp = new FactSheetsListPage(driver, logger);
+	    Assert.assertTrue(driver.getCurrentUrl().contains(FACTSHEETLISTPAGE_URL));
+		System.out.println("Fact Sheet Page URL: " + driver.getCurrentUrl());
+		}
 }
+
