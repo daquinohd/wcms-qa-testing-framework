a6c1f1c0c6f00a013fbf65a4f1a699f63428dde0
Nadia Ali <alinai@users.noreply.github.com>
2019-01-03 15:56:37 -0500

    Added tests for verifying the breadcrumb on the FactSheet List Page, … (#224)
    * Added tests for verifying the breadcrumb on the FactSheet List Page, OCEPROJECT-4988
    
    * Updated the test to remove the println() calls, OCEPROJECT-4988

--- src/main/java/gov/nci/factsheets/FactSheetsListPage.java
+++ src/main/java/gov/nci/factsheets/FactSheetsListPage.java
@@ -1,21 +1,23 @@
 package gov.nci.factsheets;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+
 import com.relevantcodes.extentreports.ExtentTest;
 
 
 
 public class FactSheetsListPage {
-    
+
 	WebDriver driver;
 	ExtentTest logger;
 
 /**************** FactSheets Page WebElements *****************************/
-	@FindBy(how = How.CSS, using = "div.resize-content>h1>span")   
+	@FindBy(how = How.CSS, using = "div.resize-content>h1>span")
 	   WebElement factsheetslist_PageTitle;
 	@FindBy(how = How.CSS, using = "#cgvBody>div.slot-item.first-SI>p")
 	    WebElement text_FSListIntroText;
@@ -41,9 +43,9 @@ public class FactSheetsListPage {
         public WebElement getPageIntroText() {
 			return text_FSListIntroText;
 		}
-}   
-		
+}
 
-			
-		
-		
+
+
+
+
