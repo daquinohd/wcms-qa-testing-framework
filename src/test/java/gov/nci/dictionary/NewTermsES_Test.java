41ef8de23177115d7a7d25a59ef6b2bea21855c8
Volker Englisch <venglisch@users.noreply.github.com>
2019-01-03 18:40:12 -0500

    GlossaryES - Test if Home page shows results for letter A. OCEPROJECT-4927

--- src/test/java/gov/nci/dictionary/NewTermsES_Test.java
+++ src/test/java/gov/nci/dictionary/NewTermsES_Test.java
@@ -241,6 +241,27 @@ public class NewTermsES_Test extends NewDictionaryCommon {
     }
 
 
+    // Confirming the A-Z list home page displays results for letter "A"
+    // -------------------------------------------------------------------------
+    @Test(dataProvider = "GlossaryES", groups = { "dictionary" })
+    public void AZListHomeIsA(String url) {
+        DictionarySearch dict;
+        String curMethod = new Object(){}.getClass().getEnclosingMethod().getName();
+
+        logger.log(LogStatus.INFO, "Testing A-Z List home page is 'A'");
+        driver.get(url);
+
+        try {
+            dict = new DictionarySearch(driver);
+            boolean azHome = dict.isAZListHomeLetterA();
+            String azNotHomeTxt = "*** Error: Glossary (ES) A-Z Home Page Not Found ***";
+            Assert.assertTrue(azHome, azNotHomeTxt);
+        } catch (MalformedURLException | UnsupportedEncodingException e) {
+            Assert.fail("*** Error loading page in " + curMethod + " ***");
+        }
+    }
+
+
     // Confirming each letter from the A-Z list can be selected and shows results
     // --------------------------------------------------------------------------
     @Test(dataProvider = "GlossaryES", groups = { "dictionary" })
